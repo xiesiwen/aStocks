b0c26b1578245bc22480258763977a0a904cef3b
xiejingwen <xiejw@133.cn>
2020-11-12 18:17:34 +0800

    修改stock页面

--- app/src/main/java/com/example/stocks/StockActivity.kt
+++ app/src/main/java/com/example/stocks/StockActivity.kt
@@ -1,8 +1,10 @@
 package com.example.stocks
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_stock.*
+import java.text.DecimalFormat
 
 /**
  * @author xiejw@133.cn
@@ -10,7 +12,8 @@ import kotlinx.android.synthetic.main.activity_stock.*
  */
 class StockActivity : Activity() {
     var ss: ArrayList<Stock> = ArrayList()
-
+    var curStock:Stock? = null
+    var b = 0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stock)
@@ -28,6 +31,8 @@ class StockActivity : Activity() {
                                 ds[0],
                                 ds[1].toFloat(),
                                 ds[2].toFloat(),
+                                ds[3].toFloat(),
+                                ds[4].toFloat(),
                                 ds[5].toFloat(),
                                 ds[6].toFloat()
                             )
@@ -36,6 +41,29 @@ class StockActivity : Activity() {
                 }
             }
             ExecUtils.runOnMainThread { stocksView.stocks = ss }
+        }
+        stop.setOnClickListener { stocksView.toggle() }
+        acc.setOnClickListener { speed.text = stocksView.acceleration() }
+        dec.setOnClickListener { speed.text = stocksView.deceleration() }
+        buy.setOnClickListener { b = curStock?.close ?: 0f }
+        sale.setOnClickListener {
+            var o = curStock?.close ?: 0f
+            if (o > b) {
+
+            } else {
+
+            }
+            stocksView.stop()
+        }
+        stocksView.setOnStockUpdate { stock ->
+            curStock = stock
+            if (stock.pct > 0) {
+                info.setTextColor(Color.RED)
+            } else {
+                info.setTextColor(Color.GREEN)
+            }
+            val format = DecimalFormat("0.##")
+            info.text = "${stock.data}  ${format.format(stock.close)}  ${if (stock.pct>0) "+" else ""}${format.format(stock.pct)}%"
         }
     }
 
--- app/src/main/java/com/example/stocks/StocksView.kt
+++ app/src/main/java/com/example/stocks/StocksView.kt
@@ -4,16 +4,21 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import java.text.DecimalFormat
 
 /**
  * @author xiejw@133.cn
  * @date 2020/11/11 11:23
  */
 class StocksView : View {
-    var paint = Paint()
+    var paint = Paint().apply {
+        textSize = 26f
+    }
     var stocks = ArrayList<Stock>()
         set(value) {
             field = value
@@ -22,12 +27,24 @@ class StocksView : View {
     var showStart = 0
     var itemWidth = 10f
     var next = {
-        showStart += 1
-        invalidate()
+        if (!touch) {
+            showStart += 1
+            currentIndex = ((width / itemWidth).toInt() + showStart).coerceAtMost(stocks.size)
+            update?.invoke(stocks[currentIndex])
+            invalidate()
+        }
         play()
     }
+    var update: ((Stock) -> Unit)? = null
+    var select: ((Stock) -> Unit)? = null
     var speed = 3000L
     var maxVol = 0f
+    var running = false
+    var currentIndex = 0
+    var path = Path()
+    val format = DecimalFormat("0.##")
+    var touch = false
+
     constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
@@ -36,85 +53,127 @@ class StocksView : View {
         paint.color = Color.RED
     }
 
-    fun play(){
-        if (width > 0 && width / itemWidth < stocks.size - showStart) {
-            postDelayed(next,speed)
+    fun play() {
+        running = if (width > 0 && width / itemWidth < stocks.size - showStart) {
+            postDelayed(next, speed)
+            true
+        } else {
+            false
         }
     }
 
-    fun stop(){
-        removeCallbacks(next)
+    fun stop() {
+        if (running) {
+            removeCallbacks(next)
+            running = false
+        }
     }
 
-    fun acceleration(){
+    fun toggle() {
+        if (running) {
+            stop()
+        } else {
+            play()
+        }
+    }
+
+    fun setOnStockUpdate(run: (Stock) -> Unit) {
+        update = run
+    }
+
+    fun setOnStockSelect(run: (Stock) -> Unit) {
+        select = run
+    }
+
+    fun acceleration(): String {
         speed -= 1000
         speed = speed.coerceAtLeast(0)
+        return (speed / 1000).toInt().toString()
     }
 
-    fun deceleration(){
+    fun deceleration(): String {
         speed += 1000
+        return (speed / 1000).toInt().toString()
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        Log.d("xsw", "ondraw size  ${stocks.size}")
         if (stocks.isEmpty()) {
             return
         }
         var volH = 200f
         var x = 0f
-        var min = stocks[showStart].close
-        var max = stocks[showStart].close
+        var minA = stocks[showStart].close
+        var maxA = stocks[showStart].close
         if (maxVol == 0f) {
             maxVol = stocks[showStart].volume
         }
-        var maxIndex = ((width/itemWidth).toInt() + showStart).coerceAtMost(stocks.size)
-        Log.d("xsw", "$showStart   $maxIndex")
-        for (i in showStart..maxIndex) {
-            min = min.coerceAtMost(stocks[i].close).coerceAtMost(stocks[i].open)
-            max = max.coerceAtLeast(stocks[i].close).coerceAtLeast(stocks[i].open)
+        if (currentIndex == 0) {
+            currentIndex = ((width / itemWidth).toInt() + showStart).coerceAtMost(stocks.size - 1)
+        }
+        for (i in showStart..currentIndex) {
+            minA = minA.coerceAtMost(stocks[i].low)
+            maxA = maxA.coerceAtLeast(stocks[i].high)
             maxVol = maxVol.coerceAtLeast(stocks[i].volume)
         }
 
         canvas?.save()
-        canvas?.clipRect(0f,0f,width.toFloat(),height-volH)
-        canvas?.scale(1f,(height-volH)/(max-min),0f,0f)
-        for (i in showStart..maxIndex) {
+        canvas?.clipRect(0f, 0f, width.toFloat(), height - volH)
+        canvas?.scale(1f, (height - volH) / (maxA - minA), 0f, 0f)
+        for (i in showStart..currentIndex) {
             if (stocks[i].close < stocks[i].open) {
                 paint.color = Color.GREEN
             } else {
                 paint.color = Color.RED
             }
-            min = stocks[i].close.coerceAtMost(stocks[i].open)
-            max = stocks[i].close.coerceAtLeast(stocks[i].open)
+            var min = stocks[i].close.coerceAtMost(stocks[i].open)
+            var max = stocks[i].close.coerceAtLeast(stocks[i].open)
             canvas?.drawRect(
                 x,
-                max - max,
+                maxA - max,
                 x + itemWidth,
-                max - min, paint
+                maxA - min, paint
             )
             canvas?.drawRect(
-                x+itemWidth/2-1,
-                max - min,
-                x + itemWidth/2+1,
-                max - max, paint
+                x + itemWidth / 2 - 1,
+                maxA - min,
+                x + itemWidth / 2 + 1,
+                maxA - stocks[i].low, paint
             )
             canvas?.drawRect(
-                x+itemWidth/2-1,
-                max - min,
-                x + itemWidth/2+1,
-                max - max, paint
+                x + itemWidth / 2 - 1,
+                maxA - stocks[i].high,
+                x + itemWidth / 2 + 1,
+                maxA - max, paint
             )
             x += itemWidth
-            if (x > width) {
-                break
-            }
         }
+        paint.color = Color.parseColor("#C71585")
+        paint.style = Paint.Style.STROKE
+        x = 0f
+        path.reset()
+        for (i in showStart..currentIndex) {
+            var p = 0f
+            if (i >= 20) {
+                for (z in i - 20..i) {
+                    p += stocks[z].close
+                }
+                p /= 20
+                if (i == 20) {
+                    path.moveTo(x + itemWidth / 2, maxA - p)
+                } else {
+                    path.lineTo(x + itemWidth / 2, maxA - p)
+                }
+            }
+            x += itemWidth
+        }
+        canvas?.drawPath(path, paint)
         canvas?.restore()
 
         x = 0f
-        for (i in showStart..maxIndex){
-            var h = (volH - 50)/maxVol*stocks[i].volume
+        paint.style = Paint.Style.FILL
+        for (i in showStart..currentIndex) {
+            var h = (volH - 50) / maxVol * stocks[i].volume
             if (stocks[i].pct > 0) {
                 paint.color = Color.RED
             } else {
@@ -128,6 +187,28 @@ class StocksView : View {
             )
             x += itemWidth
         }
+
+        paint.color = Color.parseColor("#eeeeee")
+        canvas?.drawText(format.format(maxA).toString(), 10f, 10f, paint)
+        canvas?.drawText(format.format(minA).toString(), 10f, height - volH, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                touch = true
+                selectX(event.x)
+            }
+            MotionEvent.ACTION_MOVE ->
+                selectX(event.x)
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> touch = false
+        }
+        return true
+    }
+
+    fun selectX(x: Float) {
+        var ind = ((x / itemWidth).toInt() + showStart).coerceAtMost(stocks.size - 1)
+        select?.invoke(stocks[ind])
     }
 
 }
